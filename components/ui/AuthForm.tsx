fbd7a528989a73f7e14e6441d9771c07face05b9
hussain-05 <70021411+hussain-05@users.noreply.github.com>
2024-07-08 03:25:59 +0400

    Implement plaid link and dwolla

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 
 const AuthForm = ({type}: {type: string}) => {
@@ -47,9 +48,21 @@ const AuthForm = ({type}: {type: string}) => {
 
     try {
       // Sign up with Appwrite & create plaid token
-
       if(type === 'sign-up') {
-        const newUser = await signUp(data);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+
+        const newUser = await signUp(userData);
 
         setUser(newUser);
       }
@@ -88,7 +101,7 @@ const AuthForm = ({type}: {type: string}) => {
         </Link>
 
         <div className='flex flex-col gap-1 md:gap-3'>
-          <h1>
+          <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
             {user
               ? 'Link Account'
               : type === 'sign-in'
@@ -106,7 +119,7 @@ const AuthForm = ({type}: {type: string}) => {
       </header>
       {user ? (
         <div className='flex flex-col gap-4'>
-          {/* PlaidLink */}
+          <PlaidLink user={user} variant='primary'/>
         </div>
       ): (
         <>
@@ -164,7 +177,7 @@ const AuthForm = ({type}: {type: string}) => {
                 <div className='flex gap-4'>
                   <CustomInput
                     control={form.control}
-                    name='dob'
+                    name='dateOfBirth'
                     label='Date of Birth'
                     placeholder={'yyyy-mm-dd'}
                   />
